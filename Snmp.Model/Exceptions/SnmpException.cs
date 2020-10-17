1ce829774e1e7b7d8461c8f5c3f96711c2fae1cc
Yuri Golovkin <yury.golovkin@firstlinesoftware.com>
2020-10-17 13:04:00 +0300

    added exception

---
+++ Snmp.Model/Exceptions/SnmpException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Snmp.Model.Exceptions
+{
+    public class SnmpException : Exception
+    {
+        public SnmpException() : base() { }
+
+        public SnmpException(string message) : base(message) { }
+    }
+}
