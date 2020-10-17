280f030d6e7378d3324fe0d6a0983d3ae15b96b1
Yuri Golovkin <yuri.golovkin@gmail.com>
2020-10-17 19:23:31 +0300

    added types of request and types of values

---
+++ Snmp.Model/Enums/SnmpTypeRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Snmp.Model.Enums
+{
+    public enum SnmpTypeRequest : byte
+    {
+        GetRequest,
+        SetRequest,
+        GetNextRequest,
+        GetBulkRequest,
+        Response,
+        Trap,
+        InformRequest,
+    }
+}
---
+++ Snmp.Model/Enums/SnmpValueType.cs
@@ -0,0 +1,23 @@
+﻿namespace Snmp.Model.Enums
+{
+    public enum SnmpValueType : byte
+    {
+        Integer = 0x02,
+        Integer32 = 0x02,
+        OctetString = 0x04,
+        Bits = 0x04,
+        Null = 0x05,
+        Object_Identifier = 0x06,
+        IpAddress = 0x40,
+        Counter = 0x41,
+        Counter32 = 0x41,
+        Gauge = 0x42,
+        Gauge32 = 0x42,
+        Unsigned32 = 0x42,
+        TimeTicks = 0x43,
+        Opaque = 0x44,
+        NsapAddress = 0x45,
+        Counter64 = 0x46,
+        UInteger32 = 0x47,
+    }
+}
