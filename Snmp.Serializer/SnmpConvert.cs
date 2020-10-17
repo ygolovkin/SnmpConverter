f0909c36fa9c509d352ef4a331e391eabe33790f
Yuri Golovkin <yuri.golovkin@gmail.com>
2020-10-17 13:18:57 +0300

    added splitting packet serializing

--- Snmp.Serializer/SnmpConvert.cs
+++ Snmp.Serializer/SnmpConvert.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Snmp.Model.Exceptions;
 using Snmp.Model.Packet;
 using Snmp.Serializer.CheckExtensions;
 
@@ -16,6 +17,27 @@ namespace Snmp.Serializer
         {
             packet.GeneralCheck();
 
+            return packet switch
+            {
+                SnmpPacketV1 v1 => v1.Serialize(),
+                SnmpPacketV2C v2c => v2c.Serialize(),
+                SnmpPacketV3 v3 => v3.Serialize(),
+                _ => throw new SnmpException("Incorrect packet format")
+            };
+        }
+
+        private static IEnumerable<byte> Serialize(this SnmpPacketV1 packet)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<byte> Serialize(this SnmpPacketV2C packet)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<byte> Serialize(this SnmpPacketV3 packet)
+        {
             throw new NotImplementedException();
         }
     }
