bfc0716fa4d6bef691b8dbb78de099d478ccc142
Yuri Golovkin <yury.golovkin@firstlinesoftware.com>
2020-10-17 12:57:07 +0300

    added serializer

---
+++ Snmp.Serializer/SnmpConvert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Snmp.Model.Packet;
+
+namespace Snmp.Serializer
+{
+    public static class SnmpConvert
+    {
+        public static SnmpBasePacket Serialize(this IEnumerable<byte> source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<byte> Serialize(this SnmpBasePacket packet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
