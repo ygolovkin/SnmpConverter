d3e1121ba287bce43c3a4f9693522ed78442c954
Yuri Golovkin <yuri.golovkin@gmail.com>
2023-03-25 13:48:59 +0100

    added 1 version for enum

--- SnmpConverter/Models/Enums/SnmpVersion.cs
+++ SnmpConverter/Models/Enums/SnmpVersion.cs
@@ -7,12 +7,17 @@
 public enum SnmpVersion : byte
 {
     /// <summary>
-    /// SNMP V2c version
+    /// SNMP 1 version
+    /// </summary>
+    V1 = 0x00,
+
+    /// <summary>
+    /// SNMP 2c version
     /// </summary>
     V2C = 0x01,
 
     /// <summary>
-    /// SNMP V3 version
+    /// SNMP 3 version
     /// </summary>
     V3 = 0x03
 }
